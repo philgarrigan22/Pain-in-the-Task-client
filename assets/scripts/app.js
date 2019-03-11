813e39bc477521e09823d2278f761d30e831eaa7
philgarrigan22 <philgarrigan@comcast.net>
2019-03-11 15:22:56 -0400

    Initial setup
    - Add materialize fonts / etc to index.html.
    - Add sign-up / sign-in forms to start testing.
    - Add side navbar and event listener/action.
    - Add initial scss styles.
    - Add auth files needed to start set-up/testing.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,9 +3,11 @@
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
 
+// const authevents = require('./auth/events.js')
+
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
 $(() => {
-  // your JS code goes here
+  $('.sidenav').sidenav()
 })
